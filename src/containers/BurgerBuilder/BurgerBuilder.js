f88dcbf80fa6c79188b56b70a7171e30236b0172
Abhit Anand <abhitcr1@gmail.com>
2020-06-19 02:41:52 +0530

    disabled remove button if no ingredients added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -57,12 +57,19 @@ removeIngredientHandler=(type)=>{
 
 
     render () {
+        const disabledInfo={
+            ...this.state.ingredients
+        };
+        for(let key in disabledInfo){
+            disabledInfo[key]=disabledInfo[key]<=0
+        }
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                 ingredientAdded={this.addIngredientHandler}
                 ingredientRemoved={this.removeIngredientHandler}
+                disabled={disabledInfo}
                 />
             </Aux>
         );
