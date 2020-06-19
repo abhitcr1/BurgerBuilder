48398d938e2e0a53d8e0dd7b382d224e2ce1d023
Abhit Anand <abhitcr1@gmail.com>
2020-06-19 13:35:35 +0530

    logic for disabling order button when no ingredients are added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -24,7 +24,20 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable:false
+    }
+
+    updatePurchaseState(ingredients){
+    
+        const sum=Object.keys(ingredients)
+        .map(igKey=>{
+            return ingredients[igKey];
+        })
+        .reduce((sum, el)=>{
+            return sum + el;
+        },0);
+        this.setState({purchasable: sum>0})
     }
 
 addIngredientHandler=(type)=>{
@@ -38,6 +51,7 @@ addIngredientHandler=(type)=>{
     const oldPrice=this.state.totalPrice;
     const newPrice= oldPrice+priceAddition;
     this.setState({totalPrice:newPrice,ingredients:updatedIngredients});
+    this.updatePurchaseState(updatedIngredients);
 }
 removeIngredientHandler=(type)=>{
     const oldCount=this.state.ingredients[type];
@@ -53,6 +67,7 @@ removeIngredientHandler=(type)=>{
     const oldPrice=this.state.totalPrice;
     const newPrice= oldPrice-priceDeduction;
     this.setState({totalPrice:newPrice,ingredients:updatedIngredients});
+    this.updatePurchaseState(updatedIngredients);
 }
 
 
@@ -70,6 +85,7 @@ removeIngredientHandler=(type)=>{
                 ingredientAdded={this.addIngredientHandler}
                 ingredientRemoved={this.removeIngredientHandler}
                 disabled={disabledInfo}
+                purchasable={this.state.purchasable}
                 price={this.state.totalPrice}
                 />
             </Aux>
