0a57563bae5e4bcf59659f93b37fbbef802fdd4e
Abhit Anand <abhitcr1@gmail.com>
2020-06-19 14:05:23 +0530

    added modal logic(overlaying always)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-
 import Aux from '../../hoc/Auxi';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
-
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES={
     salad:0.5,
@@ -78,7 +77,9 @@ removeIngredientHandler=(type)=>{
         }
         return (
             <Aux>
-                <Modal/>
+                <Modal> 
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                 ingredientAdded={this.addIngredientHandler}
