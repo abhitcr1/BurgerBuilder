0b8c7e8441bfef2d78d2e7dad6ce1342d1bf54db
Abhit Anand <abhitcr1@gmail.com>
2020-06-20 12:34:24 +0530

    Navigation Item

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import navigationItems from '../NavigationItems';
-import classes from './NavigationItem.css'
-import NavigationItem from './NavigationItem.NavigationItem';
+
+import classes from './NavigationItem.css';
+
 
 const navigationItem=(props)=>(
 
