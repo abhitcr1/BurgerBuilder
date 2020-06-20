3063ebdffb208d73d55133d5050a335dc12812ec
Abhit Anand <abhitcr1@gmail.com>
2020-06-20 15:25:33 +0530

    removed sidebar when backdrop is clicked

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import Aux from '../../hoc/Auxi';
 import classes from './Layout.css';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <Aux>
-       <Toolbar/>
-       <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state={
+        showSideDrawer:true
+    }
+    SideDrawerClosedHandler=()=>{
+        this.setState({showSideDrawer:false});
+    }
 
-export default layout;
+    render(){
+        
+        return(
+                 <Aux>
+                   <Toolbar/>
+                   <SideDrawer 
+                   open={this.state.showSideDrawer}
+                   closed={this.SideDrawerClosedHandler}/>
+                    <main className={classes.Content}>
+                        {this.props.children}
+                    </main>
+                </Aux>
+        );
+    }
+}
+export default Layout;
