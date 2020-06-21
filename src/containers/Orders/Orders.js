99fdc72a165dd8b64976525aa826ac1d6cc09b9d
Abhit Anand <abhitcr1@gmail.com>
2020-06-22 04:39:37 +0530

    formvalidation and styling

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,42 +1,40 @@
 import React, { Component } from 'react';
-import axios from '../../axios-orders';
+
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
-
-
-
-class Orders extends Component{
-
-    state={
-        orders:[],
-        loading:true
+class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
     }
-    componentDidMount(){
-            axios.get('/orders.json')
-            .then(res=>{
-                const fetchedOrders=[];
-                for(let key in res.data){
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
                     fetchedOrders.push({
                         ...res.data[key],
-                        id:key
+                        id: key
                     });
                 }
-                    this.setState({loading:false, orders:fetchedOrders});
+                this.setState({loading: false, orders: fetchedOrders});
             })
-            .catch(err=>{
-                this.setState({loading:false});
-            }) ;
+            .catch(err => {
+                this.setState({loading: false});
+            });
     }
-    render(){
-        return(
+
+    render () {
+        return (
             <div>
-                {this.state.orders.map(order=>(
+                {this.state.orders.map(order => (
                     <Order 
-                    key={order.id}
-                    ingredients={order.ingredients}
-                    price={order.price}
-                    />
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price} />
                 ))}
             </div>
         );
