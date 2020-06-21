5dc21bfbcade728a72e8dc7342ce6e7f090f3201
Abhit Anand <abhitcr1@gmail.com>
2020-06-22 00:08:37 +0530

    added forma and spinning redirect to home

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,18 +2,18 @@ import React, {Component}  from 'react';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 class ContactData extends Component{
 
     state:{
         name:"",
         email:"",
         address:{
-            street:"",
-            postalCode:""
-        },
+                  street:"",
+                  postalCode:""
+                    },
         loading:false
-
-  }
+    }
 
   orderHandler=(event)=>{
     event.preventDefault();
@@ -35,26 +35,32 @@ class ContactData extends Component{
          }
          axios.post( '/orders.json', order )
              .then( response => {
-                 this.setState( { loading: false, purchasing: false } );
+                 this.setState( { loading: false} );
              } )
              .catch( error => {
-                 this.setState( { loading: false, purchasing: false } );
+                 this.setState( { loading: false} );
              } );
-            }
-
+            
+ }
 render(){
-    return(
-        <div className={classes.ContactData}>
-            <h4>Enter your Contact Data</h4>
-            <form>
+    let form=(
+        <form>
                 <input className={classes.Input} type="text" name="name" placeholder="Your name" />
                 <input className={classes.Input} type="email" name="email" placeholder="Your email" />
                 <input className={classes.Input} type="text" name="street" placeholder="Your street" />
                 <input className={classes.Input} type="text" name="postal" placeholder="Your ZipCode" />
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
+    );
+    if(this.state.loading){
+        form=<Spinner/>;
+    }
+    return(
+        <div className={classes.ContactData}>
+            <h4>Enter your Contact Data</h4>
+            {form}
         </div>
     );
-}
+    }
 }
   export default ContactData;
