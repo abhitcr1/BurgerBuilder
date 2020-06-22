9e81977430f2916342a8857e52f7b212a267abb0
Abhit Anand <abhitcr1@gmail.com>
2020-06-23 04:02:14 +0530

    polished and ready for deployment build

--- src/index.js
+++ src/index.js
@@ -11,8 +11,9 @@ import registerServiceWorker from './registerServiceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
+const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
