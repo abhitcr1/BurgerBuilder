a517911f5b37e1ad0c769f710307cabed81e9fb4
Abhit Anand <abhitcr1@gmail.com>
2020-06-23 01:17:18 +0530

    dummy auth withut backend

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,8 @@ import React,{Component} from 'react';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
+import * as actions from '../../store/actions/index';
+import {connect} from 'react-redux';
 
 class Auth extends Component{
 
@@ -81,6 +83,10 @@ class Auth extends Component{
         };
         this.setState({controls:updatedControls});
     }
+    submitHandler=(event)=>{
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    }
 
 
     render(){ const formElementsArray = [];
@@ -104,7 +110,7 @@ class Auth extends Component{
         ));
         return(
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {form}
                 <Button btnType="Success">SUBMIT</Button>
                 </form>
@@ -112,5 +118,10 @@ class Auth extends Component{
         );
     }
 }
+const mapDispatchToProps=dispatch=>{
+    return{
+        onAuth:(email,password)=> dispatch(actions.auth(email,password))
+    };
+};
 
-export default Auth;
+export default connect(null,mapDispatchToProps) (Auth);
