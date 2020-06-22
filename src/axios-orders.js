82d341bd4c224f1a0cb6c126b3e248cec868285a
Abhit Anand <abhitcr1@gmail.com>
2020-06-22 22:39:37 +0530

    had to debug because evrything just crashes -_-

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://react-my-burger.firebaseio.com/'
+    baseURL: 'https://burger-71d4b.firebaseio.com/'
 });
 
 export default instance;
