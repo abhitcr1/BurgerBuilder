6d388f299005c19b023054d437df03d50261dbda
Abhit Anand <abhitcr1@gmail.com>
2020-06-23 01:42:24 +0530

    added backend token authentication

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,5 +24,20 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData={
+            email:email,
+            password: password,
+            returnsecureToken: true
+        };
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyD-AftQpgGktM1-3K5lWemOBRiGKRzBkXc', authData)
+        .then(response=>{
+            console.log(response);
+            dispatch(authSuccess(response.data));
+        })
+        .catch(err=>{ 
+            console.log(err);
+            dispatch(authFail(err));
+
+        });
     };
 };
